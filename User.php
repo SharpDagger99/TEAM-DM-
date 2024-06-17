e8daf43974685b22e9682abd74c055bc5c3390ad
MiahPat <162461042+MiahPat@users.noreply.github.com>
2024-06-17 06:59:17 -0700

    Update User.php

--- User.php
+++ User.php
@@ -5,7 +5,7 @@ $servername = "localhost";
 $username = "root";    
 $password = "";    
 $dbname = "ttc";    
-  
+    
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
