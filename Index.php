ae72c76afce824f6cb6e560f8086102b048154e2
RitchaDanica <145423853+RitchaDanica@users.noreply.github.com>
2024-06-17 07:11:09 -0700

    Update Index.php

--- Index.php
+++ Index.php
@@ -12,7 +12,7 @@
     <!-- Bootstrap Icons (Optional) -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
  
-    <style>   
+    <style>    
    
         /* Your existing CSS styles */ 
     body {
