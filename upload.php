707af1bbc8d16c833eaed69148cafa42ad45a74b
Missmata05 <162430229+Missmata05@users.noreply.github.com>
2024-06-17 07:26:59 -0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";   
-$dbname = "ttc";
+$dbname = "ttc"; 
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
