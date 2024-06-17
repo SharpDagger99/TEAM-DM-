cb2e0c73d96bda92022e27730b6a2e49d33d5a80
SharpDagger99 <162429313+SharpDagger99@users.noreply.github.com>
2024-06-17 06:50:59 -0700

    Update Admin.php

--- Admin.php
+++ Admin.php
@@ -4,7 +4,7 @@ $servername = "localhost";
 $username = "root";          
 $password = "";          
 $dbname = "ttc";       
-        
+          
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname); 
